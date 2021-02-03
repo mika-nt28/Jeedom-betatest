f019c79b97420629c50ce7900018b5637b9d5d93
Halbout Michael <michaelhalbout@gmail.com>
2021-02-03 08:48:49 +0100

    Create betatest.class.php

---
+++ core/class/betatest.class.php
@@ -0,0 +1,11 @@
+<?php
+require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+
+class betatest extends eqLogic {
+
+	
+}
+class betatestCmd extends cmd {
+	public function execute($_options = null) {
+	}
+}
